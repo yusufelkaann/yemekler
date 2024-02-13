7183ced0f25437f9176072420fb1442e734541af
Yusuf Elkaan <elkaanyusuff@gmail.com>
2024-02-13 23:05:34 +0300

    made tarif page and worked on design

--- lib/TarifPage.dart
+++ lib/TarifPage.dart
@@ -1,21 +1,45 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: camel_case_types, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 //rimport 'package:flutter/yemeklerPage.dart';
 
 
-class TarifPage extends StatefulWidget {
-  const TarifPage({super.key});
+import 'package:flutter/material.dart';
 
-  @override
-  State<TarifPage> createState() => _tarifPageState();
-}
+class TarifPage extends StatelessWidget {
+  final String dishName;
+  final String imageUrl;
 
-class _tarifPageState extends State<TarifPage> {
+  const TarifPage({Key? key, required this.dishName, required this.imageUrl}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
+      appBar: AppBar(
+        title: Text('Tarif: $dishName'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: NetworkImage(imageUrl),
+            ),
+            SizedBox(height: 20),
+            Text(
+              dishName,
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Tarif', 
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/yemeklerPage.dart
+++ lib/yemeklerPage.dart
@@ -27,10 +27,13 @@ class YemeklerPage extends StatefulWidget {
 }
 
 class _YemeklerPageState extends State<YemeklerPage> {
-  String yemek = "Karnıyarık";
-  String tahminiSure = '45 dakika';
-  String yemerkUrl =
+  String yemekKarniyarik = "Karnıyarık";
+  String tahminiSureKarniyarik = '45 dakika';
+  String yemerkUrlKarniyarik =
       'https://cdn.yemek.com/mnresize/940/940/uploads/2022/08/100-gram-kiymayla-karniyarik-one-cikan.jpg';
+  String yemekManti = "Mantı";
+  String tahminiSureManti = '40 dakika';
+  String yemekUrlManti = 'https://cdn.yemek.com/mnresize/1250/833/uploads/2017/02/kayseri-mantisi-onecikan-yeni.jpg';
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +44,8 @@ class _YemeklerPageState extends State<YemeklerPage> {
       ),
       body: ListView(
         children: [
-          YemekRow(yemek: yemek, yemekUrl: yemerkUrl, tahminiSure: tahminiSure, yemekGoster: () => yemekGoster(yemek, yemerkUrl),),
+          YemekRow(yemek: yemekKarniyarik, yemekUrl: yemerkUrlKarniyarik, tahminiSure: tahminiSureKarniyarik, yemekGoster: () => yemekGoster(yemekKarniyarik, yemerkUrlKarniyarik),),
+          YemekRow(yemek: yemekManti, yemekUrl: yemekUrlManti, tahminiSure: tahminiSureManti, yemekGoster: () => yemekGoster(yemekManti, yemekUrlManti),),
         ],
       ),
     );
@@ -49,26 +53,45 @@ class _YemeklerPageState extends State<YemeklerPage> {
   void yemekGoster(String yemek, String yemekUrl) {
   showModalBottomSheet(
     context: context,
-    builder: (context) => ListView(
-      children: [
-        Image.network(
-          yemekUrl,
-          width: 232,
-          height: 227,
-        ),
-        Text(yemek),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => TarifPage()),
-            );
-          },
-          child: Text('Tarife Git!66'),
+    builder: (context) => Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          
+          Image.network(
+            yemekUrl,
+            width: 150,
+            height: 150,
+             
           ),
+          
+          Center(
+            child: Text(
+              yemek,
+              style: TextStyle(fontSize: 24),
+            )
+          ),
+          
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => TarifPage(dishName: yemek, imageUrl: yemekUrl,)),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red, 
+              foregroundColor: Colors.black,
+            ),
+            child: Text('Tarife Git!'),
+              
+              
+          ),
+          
         ],
       ),
-    );
-  }
+    ),
+  ); 
+  }    
 }
 
 class YemekRow extends StatelessWidget {
@@ -87,27 +110,33 @@ class YemekRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.network(
-          yemekUrl,
-          width: 77,
-          height: 77,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.network(
+              yemekUrl,
+              width: 77,
+              height: 77,
+            ),
+            SizedBox(
+              width: 30,
+              child: ListTile(
+                title: Text(yemek),
+                titleTextStyle: TextStyle(color: Colors.black),
+                subtitle: Text(" $tahminiSure"),
+                subtitleTextStyle: TextStyle(color: Color.fromARGB(255, 37, 35, 35)),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_downward),
+              onPressed: yemekGoster,
+            ),
+          ],
         ),
-        SizedBox(
-          width: 30,
-          child: ListTile(
-            title: Text(yemek),
-            titleTextStyle: TextStyle(color: Colors.black),
-            subtitle: Text("Tahmini süre $tahminiSure"),
-            subtitleTextStyle: TextStyle(color: Color.fromARGB(255, 37, 35, 35)),
-          ),
-        ),
-        IconButton(
-          icon: Icon(Icons.arrow_downward),
-          onPressed: yemekGoster,
-        ),
-      ],
+      ),
     );
   }
 }
